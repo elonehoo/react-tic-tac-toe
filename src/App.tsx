2d036ee5a79cdfedf0d10b261b5ad7daf00fd384
xiaoxunyao <huchengyea@163.com>
2022-04-24 20:58:04 +0800

    fix: Preliminary completion of tic-tac-toe

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
 import React,{ useState } from 'react'
-import './App.css'
 
-class Square extends React.Component{
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
+function Square(props:any){
+  return (
+    <button
+        className="square"
+        onClick={()=>props.onClick()}
+      >
+        {props.value}
       </button>
-    );
-  }
+  )
 }
+
 class Board extends React.Component {
+
+  constructor(props:any){
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIndex:true,
+    }
+  }
+
+  handleClick(i:number){
+    const squares = this.state.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIndex ? 'X' : 'O'
+    this.setState({
+      squares:squares,
+      xIndex:!this.state.xIndex
+    });
+  }
+
   renderSquare(i:number) {
-    return <Square />;
+    return <Square
+             onClick={()=>this.handleClick(i)}
+             value={this.state.squares[i]}
+           />;
   }
   render() {
-    const status:string = 'Next player: X';
+    const winner = calculateWinner(this.state.squares)
+    let status:string
+    if (winner) {
+      status = 'Winner: ' + winner;
+    }else {
+      status = 'Next player: ' + (this.state.xIndex ? 'X' : 'O');
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -37,6 +68,26 @@ class Board extends React.Component {
       </div>
     )
   }
+}
+
+function calculateWinner(squares:Array<any>):any{
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 class App extends React.Component {
